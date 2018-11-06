2e9187140d06e8bdc4a70fe4eea938c62bdad130
Carlos Vinícius Figueiredo de Macedo <carlos.vinicius@nubeliu.com>
2018-11-06 14:43:27 -0200

    credenciais de banco, e consulta no show global status

---
+++ nodemysql/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();         
+const bodyParser = require('body-parser');
+const port = 3000; //porta padrão
+const mysql = require('mysql');
+
+//configurando o body parser para pegar POSTS mais tarde
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+	
+//definindo as rotas
+const router = express.Router();
+router.get('/', (req, res) => res.json({ message: 'Funcionando!' }));
+app.use('/', router);
+
+//inicia o servidor
+app.listen(port);
+console.log('API funcionando!');
+
+function execSQLQuery(sqlQry, res){
+  const connection = mysql.createConnection({
+    host     : '127.0.0.1',
+    port     : 3306,
+    user     : 'root',
+    password : 'GomariaDB',
+    database : 'carlos'
+  });
+ 
+  connection.query(sqlQry, function(error, results, fields){
+      if(error) 
+        res.json(error);
+      else
+        res.json(results);
+      connection.end();
+      console.log('executou!');
+  });
+}
+	
+router.get('/clientes', (req, res) =>{
+    execSQLQuery('SHOW GLOBAL STATUS', res);
+})
